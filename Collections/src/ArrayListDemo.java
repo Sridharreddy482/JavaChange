b09b5f714d499e4c4931c70ec27413cdfb6ef06b
unknown <rsreddy482@gmail.com>
2018-09-29 14:09:10 +0530

    added arrayList

---
+++ Collections/src/ArrayListDemo.java
@@ -0,0 +1,13 @@
+public class ArrayListDemo {
+public void m1(Integer i)
+{
+	System.out.println(i);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayListDemo demo=new ArrayListDemo();
+		int i=10;
+		demo.m1(i);
+	}
+
+}
